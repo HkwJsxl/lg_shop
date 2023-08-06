c86be06ddbb8b4768bd77ecefb065b73b10b4c45
hkw <hkwJsxl@gmail.com>
2023-08-06 11:40:39 +0800

    update; 隐藏邮箱密码;

--- lg_shop/settings/dev.py
+++ lg_shop/settings/dev.py
@@ -225,7 +225,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # 指定邮件后
 EMAIL_HOST = 'smtp.163.com'  # 发邮件主机
 EMAIL_PORT = 25  # 发邮件端口
 EMAIL_HOST_USER = 'hankewei0224@163.com'  # 授权的邮箱
-EMAIL_HOST_PASSWORD = 'NDRUDJHCSOJQQYPY'  # 邮箱授权时获得的密码，非注册登录密码
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")  # 邮箱授权时获得的密码，非注册登录密码
 EMAIL_FROM = 'lgShop-<hankewei0224@163.com>'  # 发件人抬头
 
 # 邮箱激活链接
