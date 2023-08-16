576befc5d8674490736c1a5e6044f2cc0940206b
hkw <hkwJsxl@gmail.com>
2023-08-16 12:29:54 +0800

    update; 项目上线配置;

---
+++ lg_shop/settings/pro.py
@@ -0,0 +1,245 @@
+import os
+import sys
+from pathlib import Path
+
+from lg_shop.apps import users
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(1, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(1, os.path.join(BASE_DIR, 'extension'))
+sys.path.insert(1, os.path.join(BASE_DIR, 'libs'))
+
+SECRET_KEY = 'django-insecure-*r2+i*-_*dz*)_as710e^c$r#!z355z70h2_@%ckkqv&-^c#y9'
+
+DEBUG = False
+ALLOWED_HOSTS = ["*", ]
+
+DNS_NAME = "127.0.0.1"
+# DNS_NAME = "www.lg.hkwpro.com"
+
+# redis相关
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
+REDIS_HOST = DNS_NAME
+REDIS_PORT = 6379
+# mysql相关
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_HOST = DNS_NAME
+MYSQL_PORT = 3306
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    "users",
+    "contents",
+    "verifications",
+    "areas",
+    "oauth",
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'lg_shop.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR.parent / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'lg_shop.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://:%s@%s:%s/0" % (REDIS_PASSWORD, REDIS_HOST, REDIS_PORT),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://:%s@%s:%s/1" % (REDIS_PASSWORD, REDIS_HOST, REDIS_PORT),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "verifications": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://:%s@%s:%s/2" % (REDIS_PASSWORD, REDIS_HOST, REDIS_PORT),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "areas": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://:%s@%s:%s/3" % (REDIS_PASSWORD, REDIS_HOST, REDIS_PORT),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = "zh-hans"
+TIME_ZONE = "Asia/Shanghai"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = False  # 关闭时区转换以后，django会默认使用TIME_ZONE作为时区
+
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR.parent, "static")
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR.parent, "static")
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {lineno} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {module} {lineno} {message}',
+            'style': '{',
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'file': {  # 向文件中输出日志
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/lgshop.log'),  # 日志文件的位置
+            'maxBytes': 300 * 1024 * 1024,
+            'backupCount': 10,
+            'formatter': 'verbose',
+            "encoding": "utf-8",  # 解决日志输出中文乱码
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', "file"],
+            'propagate': True,
+            'level': 'INFO',
+        },
+    }
+}
+
+AUTH_USER_MODEL = "users.UserInfo"
+AUTHENTICATION_BACKENDS = ['extension.authenticate.ReModelBackend']
+
+# 容联云短信
+RONGLIANYUN = {
+    "accId": '2c94811c86c00e9b0186f2873a040afa',
+    "accToken": os.environ.get("RONGLIANYUNACCTOKEN"),
+    "appId": '2c94811c86c00e9b0186f2873b0d0b01',
+    "reg_tid": 1,  # 注册短信验证码的模板ID
+    "sms_expire": 60,  # 短信有效期，单位：秒(s)
+    "sms_interval": 60,  # 短信发送的冷却时间，单位：秒(s)
+}
+
+# celery相关
+# Celery异步任务队列框架的配置项[注意：django的配置项必须大写，所以这里的所有配置项必须全部大写]
+# 任务队列的链接地址
+CELERY_BROKER_URL = 'redis://:%s@%s:%s/14' % (
+    REDIS_PASSWORD, REDIS_HOST, REDIS_PORT
+)
+# 结果队列的链接地址
+CELERY_RESULT_BACKEND = 'redis://:%s@%s:%s/15' % (
+    REDIS_PASSWORD, REDIS_HOST, REDIS_PORT
+)
+# 时区，与django的时区同步
+CELERY_TIMEZONE = TIME_ZONE
+# 防止死锁
+CELERY_FORCE_EXECV = True
+# 设置并发的worker数量
+CELERYD_CONCURRENCY = 200
+# celery的任务结果内容格式
+CELERY_ACCEPT_CONTENT = ['json', 'pickle']
+# 设置失败允许重试[这个慎用，如果失败任务无法再次执行成功，会产生指数级别的失败记录]
+# CELERY_ACKS_LATE = True
+# 每个worker工作进程最多执行500个任务被销毁，可以防止内存泄漏，500是举例，根据自己的服务器的性能可以调整数值
+# CELERYD_MAX_TASKS_PER_CHILD = 500
+# 单个任务的最大运行时间，超时会被杀死[慎用，有大文件操作、长时间上传、下载任务时，需要关闭这个选项，或者设置更长时间]
+# CELERYD_TIME_LIMIT = 10 * 60
+# 任务发出后，经过一段时间还未收到acknowledge, 就将任务重新交给其他worker执行
+CELERY_DISABLE_RATE_LIMITS = True
+
+LOGIN_URL = "/api/users/login/"
+
+# 邮箱配置
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # 指定邮件后端
+EMAIL_HOST = 'smtp.163.com'  # 发邮件主机
+EMAIL_PORT = 25  # 发邮件端口
+EMAIL_HOST_USER = 'hankewei0224@163.com'  # 授权的邮箱
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")  # 邮箱授权时获得的密码，非注册登录密码
+EMAIL_FROM = 'lgShop-<hankewei0224@163.com>'  # 发件人抬头
+# 邮箱激活链接
+EMAIL_VERIFY_URL = "http://%s:8000/api/verify/email/verification/" % DNS_NAME
+# 邮箱验证失效时间：秒
+EMAIL_VERIFY_TOKEN_EXPIRES = 60 * 30
+
+# QQ登录相关
+QQ_CLIENT_ID = '102060113'
+QQ_CLIENT_SECRET = 'iLpDPSIyVp01sVvF'
+QQ_REDIRECT_URI = 'http://%s/api/oauth/qq/callback/' % DNS_NAME
