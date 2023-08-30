4444f958b834aea9f5f3bb14f9bc0680f8eeff61
hkw <hkwJsxl@gmail.com>
2023-08-30 10:36:49 +0800

    update; goods apps 模型表迁移;

--- lg_shop/urls.py
+++ lg_shop/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('api/verify/', include(('verifications.urls', 'verifications'), namespace='verify')),
     path('api/areas/', include(('areas.urls', 'areas'), namespace='areas')),
     path('api/oauth/', include(('oauth.urls', 'oauth'), namespace='oauth')),
+    path('api/goods/', include(('goods.urls', 'goods'), namespace='goods')),
 
     # 开放静态文件
     # re_path(r'^media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT})
